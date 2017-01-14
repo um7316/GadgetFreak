5915a3e237fefff3a7eb97cdcb614c4ef2a4f51c
Urban Malc <urban.malc@gmail.com>
2017-01-14 11:32:11 +0100

    Fixed travis configuration 10

--- gadgetfreak_web/test_device.py
+++ gadgetfreak_web/test_device.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
 from django.core.files import File
 
 import StringIO
@@ -9,21 +10,24 @@ from .models import Device, ForumTopic
 
 # Create your tests here.
 class DeviceTest(TestCase):
+    def setUp(self):
+        self.u1 = User.objects.create(username='user1')
+
     def test_device_comments_no(self):
         test_img = Image.new(mode="RGBA", size=(20, 20), color=(0, 0, 0, 0))
         image_file = StringIO.StringIO()
         test_img.save(image_file, 'JPEG', quality=90)
         f = File(image_file)
 
-        d = Device(title="Test device", description="test", img_1=f, author_id=1)
+        d = Device(title="Test device", description="test", img_1=f, author=self.u1)
         d.save()
 
-        r1 = ForumTopic(device=d, name="r1", topic_type="C", contents="contents", author_id=1)
+        r1 = ForumTopic(device=d, name="r1", topic_type="C", contents="contents", author=self.u1)
         r1.save()
 
         self.assertEqual(d.get_no_comments(), 1)
 
-        r2 = ForumTopic(device=d, name="r2", topic_type="C", contents="contents", author_id=1)
+        r2 = ForumTopic(device=d, name="r2", topic_type="C", contents="contents", author=self.u1)
         r2.save()
 
         self.assertEqual(d.get_no_comments(), 2)
@@ -34,15 +38,15 @@ class DeviceTest(TestCase):
         test_img.save(image_file, 'JPEG', quality=90)
         f = File(image_file)
 
-        d = Device(title="Test device", description="test", img_1=f, author_id=1)
+        d = Device(title="Test device", description="test", img_1=f, author=self.u1)
         d.save()
 
-        r1 = ForumTopic(device=d, name="r1", topic_type="R", contents="contents", score="2.5", author_id=1)
+        r1 = ForumTopic(device=d, name="r1", topic_type="R", contents="contents", score="2.5", author=self.u1)
         r1.save()
 
         self.assertEqual(d.get_no_reviews(), 1)
 
-        r2 = ForumTopic(device=d, name="r2", topic_type="R", contents="contents", score="3.5", author_id=1)
+        r2 = ForumTopic(device=d, name="r2", topic_type="R", contents="contents", score="3.5", author=self.u1)
         r2.save()
 
         self.assertEqual(d.get_no_reviews(), 2)
@@ -53,15 +57,18 @@ class DeviceTest(TestCase):
         test_img.save(image_file, 'JPEG', quality=90)
         f = File(image_file)
 
-        d = Device(title="Test device", description="test", img_1=f, author_id=1)
+        d = Device(title="Test device", description="test", img_1=f, author=self.u1)
         d.save()
 
-        r1 = ForumTopic(device=d, name="r1", topic_type="R", contents="contents", score="2.5", author_id=1)
+        r1 = ForumTopic(device=d, name="r1", topic_type="R", contents="contents", score="2.5", author=self.u1)
         r1.save()
 
         self.assertEqual(d.get_score(), 2.5)
 
-        r2 = ForumTopic(device=d, name="r2", topic_type="R", contents="contents", score="3.5", author_id=1)
+        r2 = ForumTopic(device=d, name="r2", topic_type="R", contents="contents", score="3.5", author=self.u1)
         r2.save()
 
         self.assertAlmostEqual(d.get_score(), 3)
+
+    def tearDown(self):
+        self.u1.delete()
